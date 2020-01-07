1eafef7de76c5e2381502c806a48de346503f489
Gerda Gatling <gerdagatling@Gerdas-MacBook-Pro.local>
2020-01-07 17:32:35 -0500

    Added styling

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calc_keys')
 const display = document.querySelector('calc_display')
+const previousKeyType = calculator.dataset.previousKeyType
 
 keys.addEventListener('click', e=>{
   if(e.target.matches('button')){
@@ -10,7 +11,7 @@ keys.addEventListener('click', e=>{
     const displayedNum =display.textContent
 
     if(!action){
-      if(displayedNum === '0'){
+      if(displayedNum === '0' || previousKeyType === 'operator'){
         display.textContent=keyContent
       }else{
         display.textContent=displayedNum + keyContent
@@ -24,6 +25,10 @@ keys.addEventListener('click', e=>{
       action === 'divide'
     ){
       key.classList.add('is-depressed')
+      calculator.dataset.previousKeyType ='operator'
+      calculator.dataset.operator =displayedNum
+      calculator.dataset.operator =action
+
     }
 
     Array.from(key.parentNode.children)
@@ -38,7 +43,27 @@ keys.addEventListener('click', e=>{
     }
 
     if(action === 'calculate'){
-      console.log('equal key')
+      const firstValue = calculator.dataset.firstValue
+      const operator = calculator.dataset.operator
+      const secondValue = displayedNum
+
+      const calculate = (n1, operater, n2) =>{
+        let result =''
+
+        if (operator === 'add'){
+          result = n1+n2
+        }else if (operator==='subtract'){
+          result = n1-n2
+        }else if (operater ==='multiply'){
+          result = n1 * n2
+        }else if (operator ==='divide'){
+          result = n1 /n2
+        }
+        return result
+      }
+
+      display.textContent = calculate(firstValue, operator, secondValue)
+
     }
   }
 
