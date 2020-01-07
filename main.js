80cf7623c0a5516bb52808e2b129df18a3ef32d4
Gerda Gatling <gerdagatling@Gerdas-MacBook-Pro.local>
2020-01-07 16:41:16 -0500

    added is-depressed functionality

--- main.js
+++ main.js
@@ -1,21 +1,44 @@
 const calculator = document.querySelector('.calculator')
 const keys = calculator.querySelector('.calc_keys')
+const display = document.querySelector('calc_display')
 
 keys.addEventListener('click', e=>{
   if(e.target.matches('button')){
     const key = e.target
     const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum =display.textContent
 
     if(!action){
-      console.log('number key!')
+      if(displayedNum === '0'){
+        display.textContent=keyContent
+      }else{
+        display.textContent=displayedNum + keyContent
+      }
     }
+
     if(
       action === 'add' ||
       action === 'subtract'||
       action === 'multiply' ||
       action === 'divide'
     ){
-      console.log('operator key!')
+      key.classList.add('is-depressed')
+    }
+
+    Array.from(key.parentNode.children)
+      .forEach(k => k.classList.remove('is-depressed'))
+
+    if(action === 'decimal'){
+      display.textContent = displayedNum + '.'
+    }
+
+    if (action === 'clear'){
+      console.log('clear key')
+    }
+
+    if(action === 'calculate'){
+      console.log('equal key')
     }
   }
 
